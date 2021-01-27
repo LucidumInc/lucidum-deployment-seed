e843e21c5d7ea0632fe883cdf3292b39a35118d1
nando <nando@hex7.com>
2021-01-27 10:59:53 -0500

    cfn policy from s3

--- x_account_assume_role/cloudformation.sh
+++ x_account_assume_role/cloudformation.sh
@@ -1,15 +1,52 @@
 #!/bin/bash
 
+set -o errexit
+set -o errtrace
 
-AWS_REGION=
-AWS_PROFILE=
+
+IDEMPOTENT_BUCKET=false
+AWS_REGION=us-west-1
+AWS_PROFILE=default
 TRUST_ACCOUNT=123456789012
+S3_BUCKET=lucidum-iam-policy-cfn
+S3_KEY=lucidum_assume_role_policy.json
+STACK_NAME=lucidum-assume-role
+STACK_TEMPLATE=lucidum_assume_role.cfn
+EXISTING_STACKS=$(aws cloudformation list-stacks --query StackSummaries[*].StackName --output text 2> /dev/null)
 
 
-aws cloudformation deploy \
-  --region ${AWS_REGION} \
-  --profile ${AWS_PROFILE} \
-  --template-file lucidum_assume_role.cfn \
-  --stack-name lucidum-assume-role \
-  --parameter-overrides TrustAccount=${TRUST_ACCOUNT} \
-  --capabilities CAPABILITY_NAMED_IAM
+if [ "${IDEMPOTENT_BUCKET}" == "true" ]; then
+  S3_BUCKET=${S3_BUCKET}-$(date +%s)
+fi
+
+
+if ! aws s3 ls s3://${S3_BUCKET} &> /dev/null; then
+  echo create iam policy bucket
+  aws s3api create-bucket \
+    --bucket ${S3_BUCKET}
+fi
+
+
+echo upload iam policy
+aws s3api put-object \
+  --bucket ${S3_BUCKET} \
+  --key ${S3_KEY} \
+  --body ${S3_KEY}
+
+
+echo deploy cloudformation
+if [ "*${STACK_NAME}*" == "${EXISTING_STACKS}" ]; then
+  echo cloudformation stack ${STACK_NAME} exists. please clean and try again.
+
+else
+  aws cloudformation deploy \
+    --region ${AWS_REGION} \
+    --profile ${AWS_PROFILE} \
+    --template-file ${STACK_TEMPLATE} \
+    --stack-name ${STACK_NAME} \
+    --capabilities CAPABILITY_NAMED_IAM \
+    --parameter-overrides \
+      TrustAccount=${TRUST_ACCOUNT} \
+      S3Bucket=${S3_BUCKET} \
+      S3Key=${S3_KEY}
+fi
